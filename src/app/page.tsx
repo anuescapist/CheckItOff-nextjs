e11e2df85faab47fa6490b34dfc271f37f912aa6
anuescapist <anuescapist@gmail.com>
2023-07-20 02:14:27 +0530

    third commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 import Image from 'next/image'
+import Link from "next/link"
 
 export default function Home() {
-  return <h1>hello</h1>
+  return ( 
+  <>
+  <header>
+    <h1  className="flex justify-between items-center mb-4" >Todos</h1>
+    </header>
+  </>
+  )
 }
