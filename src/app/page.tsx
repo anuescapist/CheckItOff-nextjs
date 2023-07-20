cb9adef7f0ec69a7ef157825a333f64b26fce741
anuescapist <anuescapist@gmail.com>
2023-07-20 14:22:43 +0530

    new button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 import Image from 'next/image'
-import Link from "next/link"
+import Link from 'next/link'
+import { prisma } from './db'
 
 export default function Home() {
-  return ( 
-  <>
-  <header>
-    <h1  className="flex justify-between items-center mb-4" >Todos</h1>
+  return (
+    <>
+    <header className="flex justify-between items-center mb-4">
+      <h1 className="text-4xl">CheckItOff</h1>
+
+      <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded
+     hover:bg-slate-600 focus-within:bg-slate-700 outline-none"
+      href="/new">New
+      </Link>
     </header>
-  </>
+
+    <ul className='pl-4'>
+
+    </ul>
+    
+    </>
   )
+  
 }
