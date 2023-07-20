6fc3c0c957400575c63d377bb0afae3b720ec27e
anuescapist <anuescapist@gmail.com>
2023-07-20 18:18:47 +0530

    final code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,36 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { prisma } from './db'
+import { TodoItem } from "@/components/TodoItems"
+import { prisma } from "./db"
+import Link from "next/link"
 
 function getTodos() {
   return prisma.todo.findMany()
 }
 
+async function toggleTodo(id: string, completed: boolean) {
+  "use server"
+
+  await prisma.todo.update({ where: { id }, data: { completed } })
+}
+
 export default async function Home() {
- const todos = await getTodos()
+  const todos = await getTodos()
+
   return (
     <>
-    <header className="flex justify-between items-center mb-4">
-      <h1 className="text-4xl">CheckItOff</h1>
-
-      <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded
-     hover:bg-slate-600 focus-within:bg-slate-700 outline-none"
-      href="/new">New
-      </Link>
-    </header>
-
-    <ul className="pl-4">
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Checkitoff</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/new"
+        >
+          New
+        </Link>
+      </header>
+      <ul className="pl-4">
         {todos.map(todo => (
-          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+          <TodoItem complete={false} key={todo.id} {...todo} toggleTodo={toggleTodo} />
         ))}
       </ul>
-    
     </>
   )
-  
-}
+  }
